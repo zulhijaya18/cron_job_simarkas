5cf570fa2abb19cb935934854db5a3f21856d5d1
zulhijaya18 <32.zulhijaya@gmail.com>
2023-06-10 22:49:09 +0800

    update

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const cron = require("node-cron");
+// const cron = require("node-cron");
 const https = require("https");
 
-cron.schedule("12 * * * * *", async function () {
-  const request = https.request(
-    "https://simarkas-strapi.onrender.com/api/config",
-    (response) => {
-      let data = "";
-      response.on("data", (chunk) => {
-        data = data + chunk.toString();
-      });
+// cron.schedule("12 * * * * *", async function () {
+const request = https.request(
+  "https://simarkas-strapi.onrender.com/api/config",
+  (response) => {
+    let data = "";
+    response.on("data", (chunk) => {
+      data = data + chunk.toString();
+    });
 
-      response.on("end", () => {
-        const body = JSON.parse(data);
-        console.log(body);
-      });
-    }
-  );
+    response.on("end", () => {
+      const body = JSON.parse(data);
+      console.log(body);
+    });
+  }
+);
 
-  request.on("error", (error) => {
-    console.log("An error", error);
-  });
-
-  request.end();
+request.on("error", (error) => {
+  console.log("An error", error);
 });
+
+request.end();
+// });
