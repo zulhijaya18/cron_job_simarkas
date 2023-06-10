1ff200c8ddb6ca15f2dbf721fcfcb421c0ea417d
zulhijaya18 <32.zulhijaya@gmail.com>
2023-06-10 22:48:05 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const cron = require("node-cron");
+const https = require("https");
+
+cron.schedule("12 * * * * *", async function () {
+  const request = https.request(
+    "https://simarkas-strapi.onrender.com/api/config",
+    (response) => {
+      let data = "";
+      response.on("data", (chunk) => {
+        data = data + chunk.toString();
+      });
+
+      response.on("end", () => {
+        const body = JSON.parse(data);
+        console.log(body);
+      });
+    }
+  );
+
+  request.on("error", (error) => {
+    console.log("An error", error);
+  });
+
+  request.end();
+});
